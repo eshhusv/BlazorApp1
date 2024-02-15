ad054e092d65ce0be3e76f93c90c357aa65466f8
eshhus <ak7527203@gmail.com>
2024-02-15 18:17:17 +0200

    Добавьте файлы проекта.

---
+++ Models/Patient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Firstname { get; set; } = null!;
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Surname { get; set; } = null!;
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Lastname { get; set; } = null!;
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        public string PassportSeriesAndNumber { get; set; } = null!;
+        public DateOnly BirthDate { get; set; }
+        public string Gender { get; set; } = null!;
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; } = null!;
+        [Required]
+        [StringLength(12, MinimumLength = 12)]
+        public string PhoneNumber { get; set; } = null!;
+        [Required]
+        [StringLength(50, MinimumLength = 6)]
+        public string Email { get; set; } = null!;
+        public string MedicalCard { get; set; } = null!;
+        [Required]
+        public int InsurancePolicy { get; set; }
+        public DateOnly InsurancePolicyExpireDate { get; set; }
+
+        public virtual ICollection<Event> Events { get; set; } = new List<Event>();
+
+        public virtual MedicalCard MedicalCardNavigation { get; set; } = null!;
+    }
+}
